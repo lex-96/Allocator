52ad775b1e9312e7521f54f09899e88a8db37972
lex-96 <lex9612@gmail.com>
2018-04-11 21:03:22 +0300

    remove

--- new_delete.h
+++ new_delete.h
@@ -6,9 +6,21 @@
 
 namespace my {
 
-    extern std::size_t alloc_counter;
-    void* malloc(std::size_t size) throw (std::bad_alloc);
-    void free(void* p) noexcept;
+  std::size_t alloc_counter = 0;
+
+  void* malloc(std::size_t size)  throw (std::bad_alloc)
+  {
+    void* p = std::malloc(size);
+     ++alloc_counter;
+    return p;
+  }
+
+  void free(void* p) noexcept
+  {
+    --alloc_counter;
+    std::free(p);
+    return;
+  }
 
 } 
 
