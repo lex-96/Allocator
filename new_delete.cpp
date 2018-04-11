2f3daa572854341581751aacc5322ce3d27ccc56
lex-96 <lex9612@gmail.com>
2018-04-11 20:16:20 +0300

    add

--- new_delete.cpp
+++ new_delete.cpp
@@ -9,13 +9,13 @@ namespace my {
   void* malloc(std::size_t size)  throw (std::bad_alloc)
   {
     void* p = std::malloc(size);
-    printf("malloc: %zu %p %zu\n", ++alloc_counter, p, size);
+     ++alloc_counter;
     return p;
   }
 
   void free(void* p) noexcept
   {
-    printf("free: %zu %p\n", --alloc_counter, p);
+    --alloc_counter:
     std::free(p);
     return;
   }
